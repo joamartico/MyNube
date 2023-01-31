14fc9d69c3508b2d9d3552c71846eb565ba64e2b
Joa Marticorena <joamartico@gmail.com>
2023-01-30 22:01:36 -0300

    new api/test

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,4 +1,6 @@
+const users = [{ id: 1 }, { id: 2 }, { id: 3 }];
 
-export default async (req, res) => {
-	res.status(200).json(['1', '2', '3']);
-};
+export default function handler(_req, res) {
+	// Get data from your database
+	res.status(200).json(users);
+}
