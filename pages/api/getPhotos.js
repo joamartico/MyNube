1732cce149b0c25b4aaf9329dd340b4b12ede12c
Joa Marticorena <joamartico@gmail.com>
2023-02-02 00:39:25 -0300

    if(data.error) res 401 in getPhotos

--- pages/api/getPhotos.js
+++ pages/api/getPhotos.js
@@ -8,27 +8,25 @@ export default async function handler(req, res) {
 		return res.status(401).end();
 	}
 
-    const secret = process.env.SECRET;
+	const secret = process.env.SECRET;
 	const token = await getToken({ req, secret, encryption: true });
 	const accessToken = token.accessToken;
-	
+
 	if (!accessToken) {
 		return res.status(401).end();
 	}
 
 	let pageToken = "";
 	let photos = [];
-    const month = req.query.month;
+	const month = req.query.month;
 	const year = req.query.year;
-    
-
 
 	while (true) {
 		const response = await fetch(
 			"https://photoslibrary.googleapis.com/v1/mediaItems:search?key=AIzaSyA_SwmEbyA7SnEqkFHiA2pS3gEL_ZuPGtk",
 			{
 				method: "POST",
-				credentials: 'include',
+				credentials: "include",
 				headers: {
 					"Content-Type": "application/json",
 					Authorization: `Bearer ${accessToken}`,
@@ -53,27 +51,20 @@ export default async function handler(req, res) {
 					pageToken: pageToken,
 				}),
 			}
-		).catch(err => {
-			console.log(err)
-			res.status(401).end();
-		})
+		);
 
-		
 		const data = await response.json();
 
-		// if(data.error) {
-		// 	return res.status(401).json(data.error)
-		// }
+		if(data.error) {
+			return res.status(401).json(data.error)
+		}
 
-		
-		
 		photos = photos.concat(data.mediaItems);
 		if (!data.nextPageToken) {
 			break;
 		}
 		pageToken = data.nextPageToken;
 	}
-    
 
 	res.status(200).json(photos);
 }
