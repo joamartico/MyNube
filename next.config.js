dfb2fe6ce5f098a7ae62898708adef17b3f67f0f
Joa Marticorena <joamartico@gmail.com>
2023-02-05 21:42:52 -0300

    simple next.config.js

--- next.config.js
+++ next.config.js
@@ -1,36 +1,5 @@
-const path = require('path')
-const CopyPlugin = require('copy-webpack-plugin')
-const withPWA = require('next-pwa')
-const runtimeCaching = require('next-pwa/cache');
-
-module.exports = withPWA({
-  
-  pwa: {
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === 'development',
-    // runtimeCaching
-  },
-
-  api: {
-    responseLimit: false,
-  },
-
-  webpack: (config) => {
-    config.plugins.push(
-      new CopyPlugin({
-        patterns: [
-          {
-            from: path.join(
-              __dirname,
-              'node_modules/ionicons/dist/ionicons/svg'
-            ),
-            to: path.join(__dirname, 'public/svg'),
-          },
-        ],
-      })
-    )
-    return config
-  },
-})
+module.exports = {
+	api: {
+		responseLimit: false,
+	},
+};
